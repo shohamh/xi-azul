34e55e98890083f714b37bd2a857b9d7e9d984f6
shohamh <shohamh@hotmail.com>
2018-11-06 23:20:05 +0000

    Wrote example program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate azul;
+
+use azul::prelude::*;
+use azul::widgets::text_input::*;
+
+struct AppState {
+    text_input: TextInputState,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        AppState { text_input: TextInputState::new("Hello, world!") }
+    }
+}
+
+impl Layout for AppState {
+    fn layout(&self, info: WindowInfo<Self>) -> Dom<Self> {
+        TextInput::new()
+            .bind(info.window, &self.text_input, &self)
+            .dom(&self.text_input)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let app = App::new(AppState::default(), AppConfig::default());
+    app.run(
+        Window::new(WindowCreateOptions::default(), Css::native()).unwrap(),
+    ).unwrap();
 }
