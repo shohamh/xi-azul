f36cdc8ebd41be7a673f8e53d6c74c34654c44d1
shohamh <shohamh@hotmail.com>
2018-11-23 15:31:25 +0000

    cargo update and rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 mod editor;
 use azul::prelude::*;
-use azul::widgets::text_input::*;
 use azul::text_layout::*;
+use azul::widgets::text_input::*;
 
 #[derive(Debug)]
 struct EditorModel {
     text_input: TextInputState,
-    selections: Vec<editor::selection::Selection>
+    selections: Vec<editor::selection::Selection>,
 }
 
 impl Default for EditorModel {
     fn default() -> Self {
-        EditorModel { text_input: TextInputState::new("Hello, world!"), selections: Vec::new() }
+        EditorModel {
+            text_input: TextInputState::new("Hello, world!"),
+            selections: Vec::new(),
+        }
     }
 }
 
@@ -26,25 +29,33 @@ impl Layout for EditorModel {
     }
 }
 
-fn text_mouse_down(state: &mut AppState<EditorModel>, event: WindowEvent<EditorModel>) -> UpdateScreen {
+fn text_mouse_down(
+    state: &mut AppState<EditorModel>,
+    event: WindowEvent<EditorModel>,
+) -> UpdateScreen {
     println!("mouse down event: {:#?}", event.cursor_relative_to_item);
     UpdateScreen::Redraw
 }
 
-fn text_mouse_over(state: &mut AppState<EditorModel>, event: WindowEvent<EditorModel>) -> UpdateScreen {
+fn text_mouse_over(
+    state: &mut AppState<EditorModel>,
+    event: WindowEvent<EditorModel>,
+) -> UpdateScreen {
     println!("mouse over event: {:#?}", event.cursor_relative_to_item);
     UpdateScreen::Redraw
 }
 
-fn text_mouse_up(state: &mut AppState<EditorModel>, event: WindowEvent<EditorModel>) -> UpdateScreen {
+fn text_mouse_up(
+    state: &mut AppState<EditorModel>,
+    event: WindowEvent<EditorModel>,
+) -> UpdateScreen {
     println!("mouse up event: {:#?}", event.cursor_relative_to_item);
-    layout_text();
+    // layout_text();
     UpdateScreen::Redraw
 }
 
 fn main() {
     let app = App::new(EditorModel::default(), AppConfig::default());
-    app.run(
-        Window::new(WindowCreateOptions::default(), Css::native()).unwrap(),
-    ).unwrap();
+    app.run(Window::new(WindowCreateOptions::default(), Css::native()).unwrap())
+        .unwrap();
 }
