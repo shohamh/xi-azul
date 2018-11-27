0349f3e70700df6091aeb1bab05bf92d0c506677
shohamh <shohamh@hotmail.com>
2018-11-27 08:43:36 +0000

    integrated xi rpc library. just need to figure out how to update the UI from different xrl message callbacks

---
+++ src/editor/wheel.rs
@@ -0,0 +1,21 @@
+use azul::prelude::*;
+use azul::widgets::button::*;
+use azul::widgets::svg::*;
+
+const WHEEL_SVG: &str = include_str!("wheel.svg");
+#[derive(Debug)]
+struct Wheel<T: Layout> {
+    elements: Vec<String>,
+    svg: Option<(SvgCache<T>, Vec<SvgLayerResource>)>,
+}
+/*
+impl<T: Layout> Layout for Wheel<T> {
+    fn layout(&self, info: WindowInfo<Self>) -> Dom<Self> {
+        if let Some((svg_cache, svg_layers)) = self.svg {
+            Svg::with_layers(svg_layers).render_svg(&info.window, &svg_cache)
+        } else {
+            Button::labeled("Load SVG").dom().with_callback(load_svg)
+        }
+    }
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,98 @@
 mod editor;
+
 use azul::prelude::*;
+use std::string::String;
 use azul::text_layout::*;
 use azul::widgets::text_input::*;
+use futures::{future, Future, Stream};
+use xrl::{LineCache, Client, Frontend, FrontendBuilder, ServerResult, Update, ScrollTo, Style,
+          AvailablePlugins, UpdateCmds, PluginStarted, PluginStoped, ConfigChanged, ThemeChanged,
+          spawn};
 
 #[derive(Debug)]
-struct EditorModel {
+struct EditorUIModel {
     text_input: TextInputState,
     selections: Vec<editor::selection::Selection>,
 }
 
-impl Default for EditorModel {
+#[derive(Debug)]
+struct XiFrontend {
+    client: Client,
+    line_cache: LineCache,
+    ui_model: EditorUIModel,
+}
+
+struct XiFrontendBuilder;
+
+impl Default for EditorUIModel {
     fn default() -> Self {
-        EditorModel {
+        EditorUIModel {
             text_input: TextInputState::new("Hello, world!"),
             selections: Vec::new(),
         }
     }
 }
 
-impl Layout for EditorModel {
+impl Frontend for XiFrontend {
+    fn update(&mut self, update: Update) -> ServerResult<()> {
+        println!("received `update` from Xi core:\n{:?}", update);
+        // note that we could send requests/notifications to the core here with `self.client`
+        self.line_cache.update(update);
+        self.ui_model.text_input.text = self.line_cache.lines()
+                                                       .iter()
+                                                       .map(|line| line.clone().text)
+                                                       .collect::<Vec<String>>()
+                                                       .join("");
+        Box::new(future::ok(()))
+    }
+    fn scroll_to(&mut self, scroll_to: ScrollTo) -> ServerResult<()> {
+        println!("received `scroll_to` from Xi core:\n{:?}", scroll_to);
+        Box::new(future::ok(()))
+    }
+    fn def_style(&mut self, style: Style) -> ServerResult<()> {
+        println!("received `def_style` from Xi core:\n{:?}", style);
+        Box::new(future::ok(()))
+    }
+    fn available_plugins(&mut self, scroll_to: AvailablePlugins) -> ServerResult<()> {
+        println!(
+            "received `available_plugins` from Xi core:\n{:?}",
+            scroll_to
+        );
+        Box::new(future::ok(()))
+    }
+    fn update_cmds(&mut self, style: UpdateCmds) -> ServerResult<()> {
+        println!("received `update_cmds` from Xi core:\n{:?}", style);
+        Box::new(future::ok(()))
+    }
+    fn plugin_started(&mut self, style: PluginStarted) -> ServerResult<()> {
+        println!("received `plugin_started` from Xi core:\n{:?}", style);
+        Box::new(future::ok(()))
+    }
+    fn plugin_stoped(&mut self, style: PluginStoped) -> ServerResult<()> {
+        println!("received `plugin_stoped` from Xi core:\n{:?}", style);
+        Box::new(future::ok(()))
+    }
+    fn config_changed(&mut self, style: ConfigChanged) -> ServerResult<()> {
+        println!("received `config_changed` from Xi core:\n{:?}", style);
+        Box::new(future::ok(()))
+    }
+    fn theme_changed(&mut self, style: ThemeChanged) -> ServerResult<()> {
+        println!("received `theme_changed` from Xi core:\n{:?}", style);
+        Box::new(future::ok(()))
+    }
+}
+
+impl FrontendBuilder<XiFrontend> for XiFrontendBuilder {
+    fn build(self, client: Client) -> XiFrontend {
+        XiFrontend {
+            client: client,
+            ui_model: EditorUIModel::default(),
+            line_cache: LineCache::new()
+        }
+    }
+}
+
+impl Layout for EditorUIModel {
     fn layout(&self, info: WindowInfo<Self>) -> Dom<Self> {
         TextInput::new()
             .bind(info.window, &self.text_input, &self)
@@ -30,24 +104,24 @@ impl Layout for EditorModel {
 }
 
 fn text_mouse_down(
-    state: &mut AppState<EditorModel>,
-    event: WindowEvent<EditorModel>,
+    state: &mut AppState<EditorUIModel>,
+    event: WindowEvent<EditorUIModel>,
 ) -> UpdateScreen {
     println!("mouse down event: {:#?}", event.cursor_relative_to_item);
     UpdateScreen::Redraw
 }
 
 fn text_mouse_over(
-    state: &mut AppState<EditorModel>,
-    event: WindowEvent<EditorModel>,
+    state: &mut AppState<EditorUIModel>,
+    event: WindowEvent<EditorUIModel>,
 ) -> UpdateScreen {
     println!("mouse over event: {:#?}", event.cursor_relative_to_item);
     UpdateScreen::Redraw
 }
 
 fn text_mouse_up(
-    state: &mut AppState<EditorModel>,
-    event: WindowEvent<EditorModel>,
+    state: &mut AppState<EditorUIModel>,
+    event: WindowEvent<EditorUIModel>,
 ) -> UpdateScreen {
     println!("mouse up event: {:#?}", event.cursor_relative_to_item);
     // layout_text();
@@ -55,7 +129,36 @@ fn text_mouse_up(
 }
 
 fn main() {
-    let app = App::new(EditorModel::default(), AppConfig::default());
-    app.run(Window::new(WindowCreateOptions::default(), Css::native()).unwrap())
-        .unwrap();
+
+    // spawn Xi core
+    let (mut client, core_stderr) = spawn("xi-core", XiFrontendBuilder {});
+
+    // All clients must send client_started notification first
+    tokio::run(client.client_started(None, None).map_err(|_| ()));
+
+    // start logging Xi core's stderr
+    let log_core_errors = core_stderr
+        .for_each(|msg| {
+            println!("xi-core stderr: {}", msg);
+            Ok(())
+        })
+        .map_err(|_| ());
+
+    ::std::thread::spawn(move || { tokio::run(log_core_errors); });
+
+    // Send a request to open a new view, and print the result
+    let open_new_view = client
+        .new_view(Some("Cargo.toml".into()))
+        .map(|view_name| println!("opened new view: {}", view_name))
+        .map_err(|_| ());
+
+    tokio::run(open_new_view);
+
+    // sleep until xi-requests are received
+    //    ::std::thread::sleep(::std::time::Duration::new(5, 0));
+
+    let app = App::new(EditorUIModel::default(), AppConfig::default());
+    app.run(
+        Window::new(WindowCreateOptions::default(), Css::native()).unwrap(),
+    ).unwrap();
 }
